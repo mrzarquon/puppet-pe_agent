d00c8e71e4f3993e92faa828830501b785695f1d
Chris Barker <cbarker@puppetlabs.com>
2013-01-11 14:18:49 -0800

    adds comments and clarifies code

--- files/PUPPET_bootstrap.sh
+++ files/PUPPET_bootstrap.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 PUPPET_var_path=`/opt/puppet/bin/puppet config print vardir`
-#PUPPET_var_path="/"
 PUPPET_lock_file="${PUPPET_var_path}/state/puppetdlock"
 PUPPET_bootstrap="false"
 PUPPET_install="false"
 PUPPET_upgrade="false"
 PUPPET_master="puppet"
 
-#function to bootstrap
+# function to bootstrap
+# This is the meat of the program. This block of code is *only* called from the 
+# either the exec calling it without -b, which causes it to fork an instance of
+# itself with the -b flag, or it being called from the command line with -b.
+# Running with -b means that we want to perform the puppet run against the
+# pe_bootstrap environment that exists on the designated puppet master, as long
+# as the puppet daemon itself still isn't in the middle of a run.
 
 puppet_bootstrap(){
   loop_timeout=320
@@ -48,27 +53,12 @@ puppet_bootstrap(){
 
 
 #check if we are the forked process or the puppet triggered one
-
-# for options in $*
-#do
-#  case $options in
-#    --install=*)
-#      PUPPET_install=${*=}
-#      ;;
-#    --upgrade=*)
-#      PUPPET_upgrade=${*=}
-#      ;;
-#    --master=*)
-#      PUPPET_master=${*=}
-#      ;;
-#    --bootstrap=*)
-#      PUPPET_bootstrap=${*=}
-#      ;;
-#    *)
-#      #unknown
-#      ;;
-#  esac
-#done
+# Flag summary:
+# -u : performing an upgrade, no need to modify puppet.conf
+# -i : performing install, so assume puppet.conf needs to be managed
+# -m : provides the master that the puppet run should be done against
+# -b : indicates to perform the bootstrap, only done from this script
+#      or a commandline invocation
 
 #arg string
 arg_string=""
@@ -101,7 +91,10 @@ while getopts ":uib:m:" opt; do
   esac
 done
 
-
+# Are we bootstraping? if so, do it. If not, fork with -b to ensure the forked
+# process bootstraps for us. This allows daemonized puppet to report to the master
+# that it triggered the bootstrap, and then have the node immediately run the bootstrap
+# once the daemonized run completes.
 if [ "${PUPPET_bootstrap}" == 'false' ]
 then
   /opt/puppet/bin/PUPPET_bootstrap -b $arg_string &
