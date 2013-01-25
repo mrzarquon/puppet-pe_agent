761ed1bb86a9c2910fb7657de5683699759affa2
Chris Barker <cbarker@puppetlabs.com>
2013-01-24 16:17:30 -0800

    added 2.7.0 for pe_serverversion until i get regex working

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,8 @@
 class pe_agent::params{
 
   #below is magical regex i need to actually get working
-  $pe_serverversion = regsubst($serverversion,'^.*?\([^\d]*(\d+)[^\d]*\).*$','\0')
+  #$pe_serverversion = regsubst($serverversion,'^.*?\([^\d]*(\d+)[^\d]*\).*$','\0')
+  $pe_serverversion = "2.7.0"
   $pe_install_version = pick($::pe_env_version, hiera('pe_env_version', $pe_serverversion))
   $pe_servername = pick($::pe_env_server, hiera('pe_env_server', $servername))
   $pe_repo = pick($$::pe_env_repo, hiera('pe_env_server', $servername))
